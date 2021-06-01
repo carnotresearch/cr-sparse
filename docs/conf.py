034905b170c1b95be349f1b763e3de790aa6f87b
Shailesh Kumar <shailesh@indigits.com>
2021-06-02 00:39:18 +0530

    corrected path for cr.sparse in docs

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ import os
 import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.join(os.path.abspath(".."), 'src'))
 sys.path.append(os.path.abspath('extensions'))
 
 
