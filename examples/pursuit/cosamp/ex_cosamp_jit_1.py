b64ed88a66894f12b1309bcfb7cfb7621b32905f
Shailesh Kumar <shailesh@indigits.com>
2021-06-05 19:41:55 +0530

    cosamp made compatible with JIT

---
+++ examples/pursuit/cosamp/ex_cosamp_jit_1.py
@@ -0,0 +1,48 @@
+from functools import partial
+
+
+import jax
+import jax.numpy as jnp
+import numpy as np
+from jax import random, jit, grad
+import scipy
+
+import cr.sparse as crs
+import cr.sparse.la as crla
+from cr.sparse import pursuit
+import cr.sparse.data as crdata
+import cr.sparse.dict as crdict
+
+M = 128
+N = 256
+K = 8
+
+key = random.PRNGKey(0)
+Phi = crdict.gaussian_mtx(key, M,N)
+
+x, omega = crdata.sparse_normal_representations(key, N, K, 1)
+x = jnp.squeeze(x)
+
+
+print(crs.nonzero_indices(x))
+print(crs.nonzero_values(x))
+
+y = Phi @ x
+
+from cr.sparse.pursuit import cosamp
+
+solution =  cosamp.solve(Phi, y, K)
+
+print(solution.x_I)
+print(solution.I)
+print(f"r_norm_sqr: {solution.r_norm_sqr:.2e}, iterations: {solution.iterations}")
+print("\n\n")
+
+cosamp_solve  = jit(cosamp.solve, 
+    static_argnums=(2), 
+    static_argnames=("max_iters", "res_norm_rtol"))
+
+sol = cosamp_solve(Phi, y, K)
+print(sol.x_I)
+print(sol.I)
+print(f"r_norm_sqr: {sol.r_norm_sqr:.2e}, iterations: {sol.iterations}")
--- src/cr/sparse/_src/pursuit/cosamp.py
+++ src/cr/sparse/_src/pursuit/cosamp.py
@@ -14,7 +14,7 @@
 
 
 import jax.numpy as jnp
-from jax import vmap
+from jax import vmap, jit, lax
 
 
 from .defs import RecoverySolution
@@ -23,50 +23,78 @@ from .util import largest_indices
 
 EXTRA_FACTOR = 2
 
-def solve(Phi, y, K, max_iters=None, res_norm_rtol=1e-3):
+
+def solve(Phi, y, K, max_iters=6, res_norm_rtol=1e-3):
     """Solves the sparse recovery problem :math:`y = \Phi x + e` using Compressive Sampling Matching Pursuit
     """
+    ## Initialize some constants for the algorithm
     M, N = Phi.shape
     K2 = EXTRA_FACTOR * K
     K3 = K + K2
-    # initialize residual
-    r = y
     # Let's conduct first iteration of OMP
     # squared norm of the signal
     y_norm_sqr = y.T @ y
-    # initialize residual squared norm with the same
-    r_norm_sqr = y_norm_sqr
-    # Number of iterations
-    iterations = 0
+
     max_r_norm_sqr = y_norm_sqr * (res_norm_rtol ** 2) 
-    I = jnp.array([])
-    flags = jnp.zeros(N, dtype=bool)
-    if max_iters is None:
-        max_iters = M // 2
-    for _ in range(max_iters):
-        # compute the correlations
-        h = Phi.T @ r
-        I_2k = largest_indices(h, K2 if iterations else K3)
-        # I_3k = jnp.union1d(I, I_2k).astype(int)
-        flags = flags.at[I_2k].set(True)
-        I_3k, = jnp.where(flags)
+
+    def init():
+        # compute the correlations of atoms with signal y
+        h = Phi.T @ y
+        # Pick largest 3K indices [this is first iteration]
+        I_3k = largest_indices(h, K3)
+        # Pick corresponding atoms to form the 3K wide subdictionary
         Phi_3I = Phi[:, I_3k]
         # Solve least squares over the selected indices
         x_3I, r_3I_norms, rank_3I, s_3I = jnp.linalg.lstsq(Phi_3I, y)
         # pick the K largest indices
         Ia = largest_indices(x_3I, K)
+        # Identify indices for corresponding atoms
         I = I_3k[Ia]
+        # Corresponding non-zero entries in the sparse approximation
         x_I = x_3I[Ia]
-        # Identify corresponding atoms
+        # Form the subdictionary of corresponding atoms
         Phi_I = Phi[:, I]
         # Compute new residual
         r = y - Phi_I @ x_I
         # Compute residual norm squared
         r_norm_sqr = r.T @ r
-        iterations += 1
-        # Check for convergence
-        if r_norm_sqr < max_r_norm_sqr:
-            break
-        flags = flags.at[:].set(False)
-        flags = flags.at[I].set(True)
-    return RecoverySolution(x_I=x_I, I=I, r=r, r_norm_sqr=r_norm_sqr, iterations=iterations)
+        # Assemble the algorithm state at the end of first iteration
+        return RecoverySolution(x_I=x_I, I=I, r=r, r_norm_sqr=r_norm_sqr, iterations=1)
+
+    def iteration(state):
+        # Index set of atoms for current solution
+        I = state.I
+        # Current iteration number
+        iterations = state.iterations
+        # compute the correlations of dictionary atoms with the residual
+        h = Phi.T @ state.r
+        # Ignore the previously selected atoms
+        h = h.at[I].set(0)
+        # Pick largest 2K indices
+        I_2k = largest_indices(h, K2)
+        # Combine with previous K indices to form a set of 3K indices
+        I_3k = jnp.hstack((I, I_2k))
+        # Pick corresponding atoms to form the 3K wide subdictionary
+        Phi_3I = Phi[:, I_3k]
+        # Solve least squares over the selected indices
+        x_3I, r_3I_norms, rank_3I, s_3I = jnp.linalg.lstsq(Phi_3I, y)
+        # pick the K largest indices
+        Ia = largest_indices(x_3I, K)
+        # Identify indices for corresponding atoms
+        I = I_3k[Ia]
+        # Corresponding non-zero entries in the sparse approximation
+        x_I = x_3I[Ia]
+        # Form the subdictionary of corresponding atoms
+        Phi_I = Phi[:, I]
+        # Compute new residual
+        r = y - Phi_I @ x_I
+        # Compute residual norm squared
+        r_norm_sqr = r.T @ r
+        return RecoverySolution(x_I=x_I, I=I, r=r, r_norm_sqr=r_norm_sqr, iterations=iterations+1)
+
+    def cond(state):
+        # limit on residual norm and number of iterations
+        return jnp.logical_and(state.r_norm_sqr > max_r_norm_sqr, state.iterations < max_iters)
+
+    state = lax.while_loop(cond, iteration, init())
+    return state
