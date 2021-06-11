ffb6f3e3ccd641a741f6b65dc43196d5ed5d7429
Shailesh Kumar <shailesh@indigits.com>
2021-06-11 20:48:04 +0530

    test synthetic data

---
+++ tests/test_synthetic_data.py
@@ -0,0 +1,23 @@
+
+from jax import random
+import jax.numpy as jnp
+
+import cr.sparse.data as crdata
+import cr.sparse as crs
+
+import pytest
+
+
+def test_sparse_vector():
+    key = random.PRNGKey(0)
+    D = 10
+    K = 2
+    S = 1
+    x, omega = crdata.sparse_normal_representations(key, D, K, S)
+    x = jnp.squeeze(x)
+    assert len(omega) == K
+    support = crs.nonzero_indices(x)
+    assert len(support) == K
+
+
+
