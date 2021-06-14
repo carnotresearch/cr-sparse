8f4a533078a7b516e679e7c875e6d00783635b5f
Shailesh Kumar <shailesh@indigits.com>
2021-06-14 23:46:27 +0530

    more dict tests

---
+++ tests/dictionaries/test_sensing_matrix.py
@@ -0,0 +1,34 @@
+import pytest
+
+from jax import random
+import jax.numpy as jnp
+
+import cr.sparse.dict as crdict
+import cr.sparse as crs
+
+
+def test_gaussian_mtx():
+    key = random.PRNGKey(0)
+    M = 10
+    N = 20
+    A = crdict.gaussian_mtx(key, M, N, True)
+    B = crdict.gaussian_mtx(key, M, N, False)
+
+
+def test_rademacher_mtx():
+    key = random.PRNGKey(0)
+    M = 10
+    N = 20
+    A = crdict.rademacher_mtx(key, M, N)
+
+def test_dirac_hadamard_basis():
+    n = 16
+    A = crdict.dirac_hadamard_basis(n)
+
+def test_dirac_dct_basis():
+    n = 16
+    A = crdict.dirac_dct_basis(n)
+
+def test_dirac_hadamard_dct_basis():
+    n = 16
+    A = crdict.dirac_hadamard_dct_basis(n)
