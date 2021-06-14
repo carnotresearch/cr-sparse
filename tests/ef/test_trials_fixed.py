87f99298108b30e23d9010d7136a02b9d86394cc
Shailesh Kumar <shailesh@indigits.com>
2021-06-14 23:35:20 +0530

    test for RecoveryTrialsAtFixed_M_N

---
+++ tests/ef/test_trials_fixed.py
@@ -0,0 +1,25 @@
+import pytest
+
+import jax.numpy as jnp
+
+from functools import partial
+
+from cr.sparse.ef import RecoveryTrialsAtFixed_M_N
+
+def test():
+    Ks = jnp.array([1,2])
+
+    evaluation = RecoveryTrialsAtFixed_M_N(
+        M = 5,
+        N = 10,
+        Ks = Ks,
+        num_dict_trials = 1,
+        num_signal_trials = 2
+    )
+    # Add solvers
+    from cr.sparse.pursuit import htp
+    htp_solve_jit = partial(htp.solve_jit, normalized=False)
+
+    evaluation.add_solver('HTP', htp_solve_jit)
+    # Run evaluation
+    evaluation('record_combined_performance.csv')
