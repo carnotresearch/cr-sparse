d4a569f787ae8218668620a48ad3c923523a8e8e
Shailesh Kumar <shailesh@indigits.com>
2021-06-28 21:47:16 +0530

    projections to norm balls

---
+++ src/cr/sparse/_src/opt/projections.py
@@ -0,0 +1,31 @@
+# Copyright 2021 CR.Sparse Development Team
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import jax.numpy as jnp
+norm = jnp.linalg.norm
+
+
+from cr.sparse import norms_l2_cw
+
+def project_to_l2_ball(x, radius=1.0):
+    x_norm = norm(x)
+    factor = radius / x_norm
+    return jnp.where(x_norm > radius, factor * x, x)
+
+
+def project_to_linf_ball(x, radius=1.0):
+    abs_x = jnp.abs(x)
+    factors = jnp.maximum(abs_x, radius)
+    factors = radius / factors
+    return x * factors
