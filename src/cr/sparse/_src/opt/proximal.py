1419386470fa2e92236b09c87594ccb4cd84db67
Shailesh Kumar <shailesh@indigits.com>
2021-08-03 11:35:01 +0530

    proximal operators

---
+++ src/cr/sparse/_src/opt/proximal.py
@@ -0,0 +1,23 @@
+# Copyright 2021 CR.Sparse Development Team
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Proximal operators
+"""
+
+def prox_l1(x, t):
+    """The proximal operator for l1 norm
+    """
+     jnp.maximum(0, x-t) - jnp.maximum(0, -x - t)
+
