640e4c0f9b6742ddfb7e56e6a1ea56c6643725ec
Shailesh Kumar <shailesh@indigits.com>
2021-08-08 15:08:29 +0530

    wavedec, code refactoring

--- src/cr/sparse/wt/__init__.py
+++ src/cr/sparse/wt/__init__.py
@@ -37,19 +37,9 @@ from cr.sparse._src.wt.dyad import (
     cut_dyadic,
 )
 
-from cr.sparse._src.wt.conv import (
-    iconv,
-    aconv,
-    mirror_filter,
-)
-
-from cr.sparse._src.wt.multirate import (
-    up_sample,
-    lo_pass_down_sample,
-    hi_pass_down_sample,
-    up_sample_lo_pass,
-    up_sample_hi_pass,
-    downsampling_convolution_periodization,
+from cr.sparse._src.wt.util import (
+    # Utility functions
+    dwt_max_level,
 )
 
 from cr.sparse._src.wt.transform import (
@@ -78,38 +68,17 @@ from cr.sparse._src.wt.transform import (
     # 2D 
     dwt2,
     idwt2,
-    ## multi-level transforms
-    forward_periodized_orthogonal,
-    forward_periodized_orthogonal_jit,
-    inverse_periodized_orthogonal,
-    inverse_periodized_orthogonal_jit
 )
 
-from cr.sparse._src.wt.orth import (
-    wavelet_function,
-    scaling_function,
-    haar,
-    db4,
-    db6,
-    db8,
-    db10,
-    db12,
-    db14,
-    db16,
-    db18,
-    db20,
-    baylkin,
-    coif1,
-    coif2,
-    coif3,
-    coif4,
-    coif5,
-    symm4,
-    symm5,
-    symm6,
-    symm7,
-    symm8,
-    symm9,
-    symm10,
-    vaidyanathan,
+
+
+from cr.sparse._src.wt.multirate import (
+    up_sample,
+    downsampling_convolution_periodization,
 )
+
+
+from cr.sparse._src.wt.multilevel import (
+    wavedec
+)
+
