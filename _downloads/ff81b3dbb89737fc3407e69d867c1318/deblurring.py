4b013226659775f3766b3594a7671bafa61367d2
shailesh1729 <shailesh1729@users.noreply.github.com>
2021-09-19 08:32:17 +0000

    deploy: aa0a2b61ab35a33135a8353bf2d2c257a54a04ed

--- _downloads/ff81b3dbb89737fc3407e69d867c1318/deblurring.py
+++ _downloads/ff81b3dbb89737fc3407e69d867c1318/deblurring.py
@@ -2,6 +2,10 @@
 Image Deblurring
 ===========================
 
+.. contents::
+    :depth: 2
+    :local:
+
 This example demonstrates following features:
 
 - ``cr.sparse.lop.convolve2D`` A 2D convolution linear operator
@@ -27,6 +31,10 @@ from cr.sparse import lop
 from cr.sparse import vision
 # Solvers for sparse linear systems
 from cr.sparse import sls
+# Several thresholding functions are available in this module
+from cr.sparse import geo
+# PSNR measurement
+from cr.sparse import metrics
 # Sample images
 import skimage.data
 # Configure JAX for 64-bit computing
@@ -67,6 +75,8 @@ H = lop.jit(H)
 # ''''''''''''''''''''''''''''''''''''''''''''''''''''''
 # Apply the blurring operator to the original image 
 blurred_image = H.times(image)
+# Measure the PSNR
+print("Blurred PSNR: ", metrics.peak_signal_noise_ratio(image, blurred_image), 'dB')
 # plot the original and the blurred images
 fig, ax = plt.subplots(ncols=2, figsize=(10, 5))
 ax[0].imshow(image, cmap=plt.cm.gray)
@@ -82,6 +92,8 @@ x0 = jnp.zeros_like(blurred_image)
 # We run LSQR algorithm to deblur the image for 50 iterations
 sol = sls.lsqr(H, blurred_image, x0, max_iters=50)
 deblurred_image = sol.x
+# Measure the PSNR
+print("Deblurred PSNR: ", metrics.peak_signal_noise_ratio(image, deblurred_image), 'dB')
 # Plot the original, blurred and deblurred image
 fig, ax = plt.subplots(ncols=3, figsize=(15, 5))
 ax[0].imshow(image, cmap=plt.cm.gray)
@@ -92,3 +104,57 @@ ax[2].imshow(deblurred_image, cmap=plt.cm.gray)
 ax[2].set_title('After deblurring')
 
 print(sol)
+
+
+# %%
+# A wavelet basis for the images
+# ''''''''''''''''''''''''''''''''''''''''''''''''''''''
+# Construct the basis
+DWT_basis = lop.dwt2D(image.shape, wavelet='haar', level=3, basis=True)
+DWT_basis = lop.jit(DWT_basis)
+# Visualize the wavelet transform of the image
+coefs = DWT_basis.trans(image)
+fig, ax = plt.subplots(ncols=2, figsize=(10, 5))
+ax[0].imshow(image, cmap=plt.cm.gray)
+ax[0].set_title('Image')
+ax[1].imshow(coefs, cmap=plt.cm.gray)
+ax[1].set_title('Wavelet coefficients')
+
+
+# %%
+# Deblurring with Fast Iterative Shrinkage and Thresholding Algorithm
+# '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+# We combine the convolution operator and the wavelet basis operator 
+A = H @ DWT_basis
+# Step size for the FISTA algorithm
+step_size = 1.
+# Thresholding function for the FISTA algorithm
+threshold_func = lambda i, x : geo.soft_threshold(x, 0.02)
+# Initial guess for the wavelet coefficients matrix is all zeros
+x0 = jnp.zeros(DWT_basis.shape[1])
+# Solve the \| A x - b \|_2^2 + \lambda \| x \|_1 problem
+sol = sls.fista_jit(
+    # The combined convolution+wavelet basis operator
+    A, 
+    # The blurred image as input
+    b=blurred_image, 
+    # Initial guess for the coefficients
+    x0=x0, 
+    # Step size for the FISTA algorithm
+    step_size=1., 
+    # Thresholding function to be used for FISTA
+    threshold_func=threshold_func, 
+    # Maximum number of iterations for which the algorithm will be run
+    max_iters=50)
+print(f"Number of FISTA iterations {sol.iterations}")
+# Compute the deblurred image from the coefficients given by FISTA
+deblurred_image = DWT_basis.times(sol.x)
+# Measure the PSNR
+print("Deblurred PSNR: ", metrics.peak_signal_noise_ratio(image, deblurred_image), 'dB')
+fig, ax = plt.subplots(ncols=3, figsize=(15, 5))
+ax[0].imshow(image, cmap=plt.cm.gray)
+ax[0].set_title('Original')
+ax[1].imshow(blurred_image, cmap=plt.cm.gray)
+ax[1].set_title('After blurring')
+ax[2].imshow(deblurred_image, cmap=plt.cm.gray)
+ax[2].set_title('FISTA deblurring')
