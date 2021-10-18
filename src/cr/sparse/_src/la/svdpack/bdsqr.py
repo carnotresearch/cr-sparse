e7b31ee2fb0f5df93290390f8fe79fcc1e881880
Shailesh Kumar <shailesh@indigits.com>
2021-10-18 18:32:52 +0530

    bdsqr basic impl

---
+++ src/cr/sparse/_src/la/svdpack/bdsqr.py
@@ -0,0 +1,40 @@
+# Copyright 2021 CR.Sparse Development Team
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import NamedTuple
+import math
+
+from jax import lax, jit, vmap
+import jax.numpy as jnp
+from jax.numpy.linalg import norm
+from jax.scipy.linalg import svd
+
+def bdsqr(alpha, beta, k):
+    """Computes the SVD of the bidiagonal matrix
+    """
+    # prepare the bidiagonal matrix
+    B = jnp.zeros((k, k))
+    indices = jnp.diag_indices(k)
+    B = B.at[indices].set(alpha[:k])
+    rows, cols = indices
+    rows = rows[1:]
+    cols = cols[:-1]
+    B = B.at[(rows, cols)].set(beta[1:k])
+    print(B)
+    U, s, Vh = svd(B)
+    print(s)
+    # pick the last row of U as the bounds
+    bnd = U[-1, :]
+    print(bnd)
+    return s, bnd
