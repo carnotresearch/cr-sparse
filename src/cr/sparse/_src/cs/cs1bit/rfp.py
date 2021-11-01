b685b1a8467ebf2bbbfe96bf3f72b7499d0b7952
Shailesh Kumar <shailesh@indigits.com>
2021-11-01 12:58:43 +0530

    inner and outer loops

--- src/cr/sparse/_src/cs/cs1bit/rfp.py
+++ src/cr/sparse/_src/cs/cs1bit/rfp.py
@@ -75,51 +75,62 @@ class RFPState(NamedTuple):
 def f_dash(x):
     return jnp.where(x <= 0, x, 0)
 
-def rfp(Phi, y, x0, lambda_=1., delta=0.1, max_iters=100):
+def rfp(Phi, y, x0, lambda_=1., delta=0.1, inner_iters=200, outer_iters=20):
     """Solver fo 1-bit compressive sensing with renormalized fixed points iteration
     algorithm     
     """
     tau = delta / lambda_
     print(f'shrinkage threshold: {tau}')
 
-    def init_fun():
-        return RFPState(x=x0, x_prev=jnp.zeros_like(x0), iterations=0)
+    def rfpi_inner(x0, lambda_):
+        def init_fun():
+            return RFPState(x=x0, x_prev=jnp.zeros_like(x0), iterations=0)
 
-    def body_fun(state):
-        # STEP-3
-        # compressive measurement based on current estimate of x
-        measurement = Phi @ state.x
-        # see which bits have same sign, which bits have different sign
-        correlation = y * measurement
-        # Only the bits which are wrong are important for further consideration
-        problems = jnp.where(correlation <= 0, -correlation, 0)
-        # multiply with the bit vector Y^T (f'(term))
-        problems = y * problems
-        # correlate with columns of Phi
-        f_bar = Phi.T @ problems
-        # STEP-4
-        # Gradient Projection on Sphere Surface
-        f_tilde = f_bar - jnp.vdot(f_bar,  state.x) *  state.x 
-        # One-sided quadratic gradient descent
-        h = state.x - delta * f_tilde
-        #print(jnp.sum(jnp.abs(h)>0.1))
-        # Shrinkage (l1 gradient descent):
-        u = shrink(h, tau)
-        # Normalization
-        x_hat = u / norm(u)
-        return RFPState(x=x_hat, x_prev=state.x, iterations=state.iterations+1)
+        def body_fun(state):
+            # STEP-3
+            # compressive measurement based on current estimate of x
+            measurement = Phi @ state.x
+            # see which bits have same sign, which bits have different sign
+            correlation = y * measurement
+            # Only the bits which are wrong are important for further consideration
+            problems = jnp.where(correlation <= 0, -correlation, 0)
+            print(f'number of wrong signs: {jnp.sum(correlation < 0)}, {jnp.sum(problems)}')
+            # multiply with the bit vector Y^T (f'(term))
+            problems = y * problems
+            # correlate with columns of Phi
+            f_bar = Phi.T @ problems
+            print(f'f_bar: {norm(f_bar)}')
+            # STEP-4
+            # Gradient Projection on Sphere Surface
+            f_tilde = f_bar - jnp.vdot(f_bar,  state.x) *  state.x 
+            print(f'f_tilde: {norm(f_tilde)}, {jnp.max(jnp.abs(f_tilde))}')
+            # One-sided quadratic gradient descent
+            h = state.x - delta * f_tilde
+            #print(jnp.sum(jnp.abs(h)>0.1))
+            # Shrinkage (l1 gradient descent):
+            u = shrink(h, tau)
+            # Normalization
+            x_hat = u / norm(u)
+            return RFPState(x=x_hat, x_prev=state.x, iterations=state.iterations+1)
 
-    def cond_fun(state):
-        # limit on number of iterations
-        a = state.iterations < max_iters
-        diff = state.x - state.x_prev
-        d_norm = norm(diff)
-        #print(f'd_norm={d_norm}')
-        b = d_norm > 1e-4
-        return jnp.logical_and(a, b)
+        def cond_fun(state):
+            # limit on number of iterations
+            a = state.iterations < inner_iters
+            diff = state.x - state.x_prev
+            d_norm = norm(diff)
+            #print(f'd_norm={d_norm}')
+            b = d_norm > 1e-4
+            return a
+            return jnp.logical_and(a, b)
 
-    state = init_fun()
-    while cond_fun(state):
-        state = body_fun(state)
-    # state = lax.while_loop(cond, body, init())
+        state = init_fun()
+        while cond_fun(state):
+            state = body_fun(state)
+        # state = lax.while_loop(cond, body, init())
+        return state
+    
+    for i in range(outer_iters):
+        state = rfpi_inner(x0, lambda_)
+        x0 = state.x
+        lambda_ = lambda_ * 1.5
     return state
