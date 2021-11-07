b60f5d6dcd47e8803621496e28a9adb4f200f523
Shailesh Kumar <shailesh@indigits.com>
2021-11-07 14:03:25 +0530

    better handling of array inputs to linear operators

--- src/cr/sparse/_src/lop/util.py
+++ src/cr/sparse/_src/lop/util.py
@@ -16,6 +16,7 @@ import jax
 from jax import random
 import jax.numpy as jnp
 
+import cr.sparse as crs
 
 def to_matrix(A):
     """Converts a linear operator to a matrix"""
@@ -25,7 +26,7 @@ def to_matrix(A):
     I = jnp.eye(n)
     times = A.times
     if A.input_ndim > 1:
-        times = lambda x: A.times(x).flatten()
+        times = lambda x: A.times(jnp.reshape(x, A.input_shape)).flatten()
     return jax.vmap(times, (1), (1))(I)
 
 def to_adjoint_matrix(A):
@@ -36,7 +37,7 @@ def to_adjoint_matrix(A):
     I = jnp.eye(m)
     trans =  A.trans
     if A.output_ndim > 1:
-        trans = lambda x: A.trans(x).flatten()
+        trans = lambda x: A.trans(jnp.reshape(x, A.output_shape)).flatten()
     return jax.vmap(trans, (1), (1))(I)
 
 
@@ -55,13 +56,14 @@ def dot_test_real(key, A, tol=1e-6):
     m, n = A.shape
     #print(f"{m=}, {n=}")
     keys = random.split(key, 2)
-    u = random.normal(keys[0], (n,))
-    v = random.normal(keys[1], (m,))
+    u = random.normal(keys[0], A.input_shape)
+    v = random.normal(keys[1], A.output_shape)
     y = A.times(u)
     x = A.trans(v)
     yy = jnp.vdot(y, v)
     xx = jnp.vdot(u, x)
-    return jnp.abs(yy - xx) / ((yy + xx + 1e-15) / 2) < tol 
+    rel_gap = jnp.abs(yy - xx) / ((yy + xx + 1e-15) / 2)
+    return rel_gap < tol 
 
 
 def dot_test_complex(key, A, tol=1e-6):
@@ -83,6 +85,20 @@ def dot_test_complex(key, A, tol=1e-6):
     imag_flag = jnp.abs(yyi - xxi) / ((yyi + xxi + 1e-15) / 2) < tol 
     return real_flag & imag_flag
 
+def rdot_test_complex(key, A, tol=1e-6):
+    """Performs a dot test on the linear operator A with complex inputs where the inner produt is Re(y^H x)"""
+    m, n = A.shape
+    keys = random.split(key, 4)
+    u = random.normal(keys[0], A.input_shape) + 1j * random.normal(keys[1], A.input_shape)
+    v = random.normal(keys[2], A.output_shape)
+    y = A.times(u)
+    x = A.trans(v)
+    yy = crs.arr_rdot(y, v)
+    xx = crs.arr_rdot(u, x)
+
+    rel_gap = jnp.abs(yy - xx) / ((yy + xx + 1e-15) / 2)
+    return rel_gap < tol 
+
 
 def apply_along_axis(times1d, trans1d, axis):
     """
@@ -90,10 +106,12 @@ def apply_along_axis(times1d, trans1d, axis):
     so that they can apply along a specific axis of an n-dim array
     """
     def times(x):
+        x = jnp.asarray(x)
         if x.ndim == 1:
             return times1d(x)
         return jnp.apply_along_axis(times1d, axis, x)
     def trans(x):
+        x = jnp.asarray(x)
         if x.ndim == 1:
             return trans1d(x)
         return jnp.apply_along_axis(trans1d, axis, x)
