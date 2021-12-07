3b22e51f7e6a8e5b5fbaad4f718a7a96eda8d2a2
Shailesh Kumar <shailesh@indigits.com>
2021-12-07 13:50:09 +0530

    test prox_l2

---
+++ tests/opt/proximal/test_l2.py
@@ -0,0 +1,16 @@
+from proximal_setup import *
+
+@pytest.mark.parametrize("x", [
+    [3,4, 5, 0], 
+    [[2,4], [3,5]]
+])
+def test_l2(x):
+    prox = proximal_ops.prox_l2()
+    assert_array_equal(prox.func(x), crs.arr_l2norm(x))
+    t = 1. 
+    px = prox.prox_op(x, t)
+    assert crs.arr_l2norm(px) <= crs.arr_l2norm(x)
+    px2, v2 = prox.prox_vec_val(x, t)
+    assert_array_equal(px, px2)
+    assert_array_equal(v2, prox.func(px2))
+
